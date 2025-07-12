560c919b74eaefecfbf641da6e462b4c87ecd7ac
PJ Miranda <pendelhaven3@gmail.com>
2025-07-12 08:56:55 +0800

    Update version to 1.4.5

--- src/main/java/com/jchs/expensestracker/controller/StageController.java
+++ src/main/java/com/jchs/expensestracker/controller/StageController.java
@@ -104,7 +104,7 @@ public class StageController {
 	}
 
 	public void setTitle(String title) {
-		stage.setTitle("Expenses Tracker - 1.4.4 - " + title);
+		stage.setTitle("Expenses Tracker - 1.4.5 - " + title);
 	}
 
 	public void showExpensesListScreen() {
